27d85db2aa4e83415fb84af15fe805f63d38db05
ABHINAV JONNADA <30497847+abhinavjonnada82@users.noreply.github.com>
2018-11-17 23:53:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ import dash_html_components as html
 import smtplib
 from abc import ABC, abstractmethod
 
-
-reddit = praw.Reddit(client_id="jGPKzTuBE-Z95w",
-                     client_secret="ml-vWi1dPEGd8NUtO70J_9yYFdY",
-                     user_agent= "bot /u/kingNAV82",
-                     username="kingNAV82",
-                     password="apple123")
+reddit = praw.Reddit(client_id="=",
+                     client_secret="",
+                     user_agent= "",
+                     username="",
+                     password="")
 
 listScore = {}
 commentScore = {}
